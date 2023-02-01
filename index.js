735fc04a8da30c2d5cfa9ae94b89ba31104afcf3
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-01 07:31:10 -0600

    Finalized Basics of control flow & return statements

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (location) {
+    return Math.abs(location-42);
+};
+
+function distanceFromHqInFeet (feetLocale) {
+    return distanceFromHqInBlocks(feetLocale)*264;
+};
+
+function distanceTravelledInFeet (starting, destination) {
+    return Math.abs(starting-destination)*264;
+};
+
+function calculatesFarePrice (starting, destination) {
+    if (distanceTravelledInFeet (starting, destination) < 400) {
+        return 0;}
+    if (distanceTravelledInFeet (starting, destination) <= 2000) {
+        return (distanceTravelledInFeet (starting, destination)-400)*.02}
+    if (distanceTravelledInFeet (starting, destination) <= 2500) {
+        return 25}
+    if (distanceTravelledInFeet (starting, destination) > 2500) {
+        return 'cannot travel that far'} 
+};
